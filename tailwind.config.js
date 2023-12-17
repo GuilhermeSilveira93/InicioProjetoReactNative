0d31dc9dfe77a4152d22aa248ad93bd95c544d02
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2023-12-16 21:28:56 -0300

    1

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        // eslint-disable-next-line prettier/prettier
+        dark: {
+          1: 'rgb(0, 255, 159)',
+          2: '#465DFF',
+          3: '#293541',
+        },
+        // eslint-disable-next-line prettier/prettier
+        light: {
+          1: 'rgb(0, 255, 159)',
+          2: '#465DFF',
+          3: '#293541',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
