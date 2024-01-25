88d755f3772c90ea135cad7bd20ab78cd85f2f45
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-01-25 16:59:08 -0300

    1.0

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,17 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
-  };
-};
+    plugins: [
+      'expo-router/babel',
+      'nativewind/babel',
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+  }
+}
