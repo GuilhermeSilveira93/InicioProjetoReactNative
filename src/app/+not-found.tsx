c4b63cc4179564078ae9f22b16d0b22b339d6bfc
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-03-10 21:38:34 -0300

    Update +not-found.tsx

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -6,10 +6,10 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
       <View className="flex-1 items-center justify-center p-5">
-        <Text className="text-lg font-bold">This screen doesn't exist.</Text>
+        <Text className="text-lg font-bold">Esta pagina não existe.</Text>
 
         <Link href="/">
-          <Text className="text-sm text-blue-500">Go to home screen!</Text>
+          <Text className="text-sm text-blue-500">Volte para Home!</Text>
         </Link>
       </View>
     </>
