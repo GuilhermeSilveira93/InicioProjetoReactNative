9b557a3998b724a99a9ee71ff581f7b94a16eb96
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-07-03 09:19:06 -0300

    1.0

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
+import { CustomInput } from '@/components';
 import React from 'react'
 import {View, Text} from 'react-native';
 export const Home = () => {
+  const form = useFo
   return (
     <View>
       <Text>Olá !</Text>
       <Text>Comece seu projeto comigo !</Text>
+      <CustomInput />
     </View>
   )
 }
